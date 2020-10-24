9904128844b9338ec1f6f90bd2bfdecd5fbbec64
2170987947 <2170987947@qq.com>
2020-10-24 17:30:13 +0800

    实现一个函数，判断一个数是不是素数。
    利用上面实现的函数打印100到200之间的素数。

--- 2020_1024/2020_1024/test.c
+++ 2020_1024/2020_1024/test.c
@@ -1,7 +1,28 @@
 #include<stdio.h>
+#include<math.h>
+//实现一个函数，判断一个数是不是素数。
+int isPrime(int n){
+	for (int i = 2; i <= sqrt(n); i++)
+	if (n%i == 0){
+		return -1;
+	}
+	return 1;
+}
 
+//利用上面实现的函数打印100到200之间的素数。
 int main(){
-	printf("Hello World!\n");
+	for (int i = 100; i <= 200; i++){
+		int ret = isPrime(i);
+		if (ret == 1){
+			printf("%d\n", i);
+		}
+	}
 	system("pause");
 	return 0;
 }
+
+//int main(){
+//	printf("Hello World!\n");
+//	system("pause");
+//	return 0;
+//}
